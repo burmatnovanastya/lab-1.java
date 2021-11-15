532a85af1b2fece970170c5444a30ab6d8d09331
burmatnovanastya <91246055+burmatnovanastya@users.noreply.github.com>
2021-11-15 17:08:00 +0300

    second

--- lab 1(1).java
+++ lab 1(1).java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Ведите число месяца: ");
+        System.out.print("Введите число месяца: ");
         int number = s.nextInt();
         if (number == 1 || number == 2 || number == 12){
             System.out.println("Зима");
