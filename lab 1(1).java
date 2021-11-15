84c9daa8e8eebc780f48c2d0fde87b7b4c97e61c
burmatnovanastya <burmatnova.nastya@mail.ru>
2021-11-15 09:03:56 +0300

    second

---
+++ lab 1(1).java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Ведите число месяца: ");
+        int number = s.nextInt();
+        if (number == 1 || number == 2 || number == 12){
+            System.out.println("Зима");
+        } else if (number == 3 || number == 4 || number == 5) {
+            System.out.println("Весна");
+        } else if (number == 6 || number == 7 || number == 8) {
+            System.out.println("Лето");
+        } else if (number == 9 || number == 10 || number == 11) {
+            System.out.println("Осень");
+        }
+    }
+}
